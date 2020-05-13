f870aa2aca4d4536c00c7780ca31cf7a0b82f3da
Abhijit Singh <absingh927@gmail.com>
2020-05-13 03:02:52 -0400

    adding basic task ui

---
+++ client/src/tasks/TaskList/Task.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+// icon imports
+import CritcalIcon from "../../assets/icons/16/icons-16-emptyTask-critical.png";
+import CritcalIcon2x from "../../assets/icons/16/icons-16-emptyTask-critical@2x.png";
+import CritcalIcon3x from "../../assets/icons/16/icons-16-emptyTask-critical@3x.png";
+
+// import Empty from "../../assets/icons/16/icons-16-emptyTask.png";
+// import Empty2x from "../../assets/icons/16/icons-16-emptyTask@2x.png";
+// import Empty3x from "../../assets/icons/16/icons-16-emptyTask@3x.png";
+
+// import Roi from "../../assets/icons/16/icons-16-emptyTask-roi.png";
+// import Roi2x from "../../assets/icons/16/icons-16-emptyTask-roi@2x.png";
+// import Roi3x from "../../assets/icons/16/icons-16-emptyTask-roi@3x.png";
+
+import RightArrow from "../../assets/icons/16/icons-16-rightarrowSmall.png";
+import RightArrow2x from "../../assets/icons/16/icons-16-rightarrowSmall@2x.png";
+import RightArrow3x from "../../assets/icons/16/icons-16-rightarrowSmall@3x.png";
+
+const TaskContainer = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+`;
+const StatusIcon = styled.div`
+  width: 10%;
+  text-align: center;
+  align-self: baseline;
+`;
+
+const TaskInfo = styled.div`
+  width: 90%
+  padding-left: 0.5rem;
+  font-weight: 800;
+  color: #2b2928;
+`;
+
+const TaskDue = styled.div`
+  color: #ff602b;
+  display: flex;
+  padding-top: 0.25rem;
+
+  img {
+    margin-right: 0.25rem;
+  }
+`;
+
+const Task = () => {
+  return (
+    <TaskContainer>
+      <StatusIcon>
+        <img
+          src={CritcalIcon}
+          srcSet={`${CritcalIcon2x} 2x, ${CritcalIcon3x} 3x`}
+          alt="CritcalIcon"
+        />
+      </StatusIcon>
+      <TaskInfo>
+        Inventory Count
+        <TaskDue>
+          <img
+            src={RightArrow}
+            srcSet={`${RightArrow2x} 2x, ${RightArrow3x} 3x`}
+            alt="RightArrow"
+          />
+          Due Today
+        </TaskDue>
+      </TaskInfo>
+    </TaskContainer>
+  );
+};
+
+export default Task;
--- client/src/tasks/TaskList/TaskListView.tsx
+++ client/src/tasks/TaskList/TaskListView.tsx
@@ -1,42 +1,75 @@
 import React from "react";
 import styled from "styled-components";
 import { darken } from "polished";
+import Task from "./Task";
 
 const Container = styled.div`
   background: #fff;
-  padding: 2rem;
+  padding: 1rem;
+  width: 100%;
 `;
 
 const ListViewHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  display: block;
 `;
 
-const MyTaskBtn = styled.div`
+const MyTaskBtn = styled.button`
   background-color: #8e3cc9;
   border: 1px solid #8e3cc9;
   color: #fff;
   padding: 0.5rem 0.75rem;
   text-align: center;
-  border-radius: 0.25rem;
+  border-radius: 0.5rem;
   cursor: pointer;
+  display: block;
 
   &:hover {
     background-color: ${darken("0.1", "#8e3cc9")};
   }
 `;
 
+const StatusHeader = styled.div`
+  margin: 1rem 0;
+`;
+
+const StatusBtn = styled(MyTaskBtn)<{ isCritical?: boolean }>`
+  background-color: ${(props) => (props.isCritical ? "#ff602b" : "#2b2928")};
+  border-color: ${(props) => (props.isCritical ? "#ff602b" : "#2b2928")};
+  padding: 0.25rem 0.75rem;
+  font-size: 0.75rem;
+
+  &:hover {
+    background-color: none;
+  }
+`;
+
+const Wrapper = styled.div`
+  margin: 1rem 0;
+`;
+
+const renderTasks = () => {
+  return (
+    <>
+      <StatusHeader>
+        <StatusBtn isCritical={true}>Critical 2</StatusBtn>
+      </StatusHeader>
+      <Task />
+      <Task />
+      <Task />
+    </>
+  );
+};
+
 const TaskListView = () => {
-  // Header
   // Progress Bar
-  // loop of tasks, by importance
   return (
     <Container>
       <ListViewHeader>
-        <MyTaskBtn>My Tasks</MyTaskBtn>
+        <Wrapper>
+          <MyTaskBtn>My Tasks</MyTaskBtn>
+        </Wrapper>
+        <Wrapper>{renderTasks()}</Wrapper>
       </ListViewHeader>
-      {/* Progress Bar */}
     </Container>
   );
 };
